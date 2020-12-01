c32d1008b6ffdc2c7b07cf480198aef42e534c95
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-01 20:46:01 +0000

    Automatically backed up by Learn

--- src/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("input")[1].addEventListener("click", function(event){
-    let desc = document.querySelectorAll("input")[0];
-    document.getElementById("list")
-    .innerText = desc.innerHTML;
-    event.preventDefault();
-  })
-});
