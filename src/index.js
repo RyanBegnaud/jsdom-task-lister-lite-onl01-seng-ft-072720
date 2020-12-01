e79cbdd8c5d80602f4e4fedc5d2c49de44f08398
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-01 21:00:23 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let sub = document.querySelector('#create-task-form').addEventListener("submit", function(e){
   e.preventDefault();
   let des = document.querySelector('input[name="new-task-description"]')
-  
+  let tasks = document.querySelector("#tasks")
 })
